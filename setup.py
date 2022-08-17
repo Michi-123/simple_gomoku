efae5deb5524c6203c87a66244195c4c9b60c51d
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2022-08-18 08:00:06 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(current_path, 'README.md'), encoding='utf-8') as f:
 setup(
     name='simple_gomoku', 
     packages=find_packages(exclude=('*.pyc',)),
-    version='1.1.6',
+    version='1.1.7',
     license='MIT', 
     install_requires=[],
     author='M.Uchiyama',
