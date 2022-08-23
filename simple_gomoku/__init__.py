7824f021546db5190b97d9780bf711340c1e2db4
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2022-08-23 10:15:07 +0900

    Update __init__.py

--- simple_gomoku/__init__.py
+++ simple_gomoku/__init__.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append('./')
 
-from .SelfPlay import *
-from .MCTS import *
-from .Train import *
-from .Util import *
-from .Evaluate import *
+# from .SelfPlay import *
+# from .MCTS import *
+# from .Train import *
+# from .Util import *
+# from .Evaluate import *
